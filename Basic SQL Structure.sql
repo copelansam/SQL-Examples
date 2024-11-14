223cac84e9b264850f413135f4c35c1643a62e52
copelansam <copelansam@gmail.com>
2024-11-13 21:13:55 -0500

    Create Basic SQL Structure.sql

---
+++ Basic SQL Structure.sql
@@ -0,0 +1,3 @@
+SELECT * FROM Customers /*Retrieves all rows and columns from table 'Customers'*/
+SELECT FirstName, LastName, Age FROM Customers /* Displays the FirstName, LastName, and Age columns of all rows in table 'Customers' */
+SELECT FirstName, LastName FROM Customers WHERE Age > 18 /*Retrieve the FirstName and LastName columns of all rows in the table 'Customers' that have an age field that is above 18*/
